79cbac380da39d38abb420bd3b05cc744cbdb617
Javy-He <javy.he@outlook.com>
2024-06-11 02:53:11 -0600

    Created BaroTask, implemented LPS22HH driver, ready to integrate MS5611 driver

---
+++ CM7/Components/Data.hpp
@@ -0,0 +1,35 @@
+/*
+ * Data.hpp
+ */
+
+#ifndef DATA_HPP_
+#define DATA_HPP_
+
+#include "SystemDefines.hpp"
+
+/*
+ *  mario  = Experiment Board LPS22HH U3
+ *  luigi  = Experiment Board LPS22HH U4
+ *  bowser = Main Board MS5611 U4
+ */
+typedef struct BarometerData {
+    float marioPressure;
+    float marioTemperature;
+    float luigiPressure;
+    float luigiTemperature;
+    // float bowserPressure;
+    // float bowserTemperature;
+} BarometerData;
+
+/*
+ * Data Containers
+ * Acts as a pointer to the other data structs alongside a timestamp for logging to flash.
+ */
+typedef struct AllData {
+	// IMUData*         	 imuData;
+	// AltimeterData* 		 altimeterData;
+    BarometerData*       barometerData;
+    uint32_t 			 time;
+} AllData;
+
+#endif /* DATA_HPP_ */
